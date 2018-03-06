904bcdf6c77537abed9da265b9b5453cdf6e38cf
Peter Tsrunchev <ptsrunch@lxplus008.cern.ch>
2018-03-06 14:27:20 +0100

    pcc const correction

--- Scripts/pcc_const.py
+++ Scripts/pcc_const.py
@@ -2,7 +2,7 @@ import pickle as pkl
 
 old = '/afs/cern.ch/user/p/ptsrunch/LiteVdM/Fill6016_Jul282017/LuminometerData/Rates_PCC_6016_old.pkl'
 new = '/afs/cern.ch/user/p/ptsrunch/LiteVdM/Fill6016_Jul282017/LuminometerData/Rates_PCC_6016.pkl'
-const = 0.35
+const = 0.8
 
 with open(old,'r') as f:
     data = pkl.load(f)
