6c7a508405d739afe26c754dd270143b2c3190cf
ptsrunch <peter.tsrunchev@cern.ch>
2018-04-02 20:38:47 +0200

    background comparison working with correction

--- Scripts/BGComparison.py
+++ Scripts/BGComparison.py
@@ -1,18 +1,22 @@
 import json
 import os
 import pandas
+import numpy as np
 
-folder = '/brildata/vdmoutput/AutomationBackground/Analysed_Data/'
+folder = '/brildata/vdmoutput/AutomationBackgroundCorrection2/Analysed_Data/6016_28Jul17_100134_28Jul17_102201/'
 luminometers = ['PLT', 'BCM1FPCVD', 'HFOC']
 data = {}
-for l in luminometers:
-    data[l] = []
-    data[l+'Err'] = []
-for scanfolder in os.listdir(folder):
+for ratefile in os.listdir(folder + 'LuminometerData/'):
     for luminometer in luminometers:
-        with open(folder + scanfolder + '/LuminometerData/Rates_' + luminometer + '_6016.json' ) as f:
+        data[luminometer] = {}
+        with open(folder + 'LuminometerData/Rates_' + luminometer + '_6016.json' ) as f:
             j = json.load(f)
-            data[luminometer].append(j['backgroundApplied'])
-            data[luminometer+'Err'].append(j['backgroundError'])
+            print j['Scan_1'][13]
+            data[luminometer]['Rate'] = np.mean(j['Scan_1'][12]['Rates'].values())
+    for luminometer in luminometers:
+        with open(folder + '/corr/Background_' + luminometer + '_6016.json' ) as f:
+            j = json.load(f)
+            data[luminometer]['Background'] = j['background']
+            data[luminometer]['BackgroundError'] = j['backgroundError']
 
-pandas.DataFrame.from_dict(data).to_csv('bgcomparison.csv')
+pandas.DataFrame.from_dict(data).to_csv('bg6016scan1.csv')
